818170626dda7846dc220b642d0cf02f39625625
Tamagil <tamagiltamagil@gmail.com>
2024-10-14 18:51:33 +0300

    feat:added redis settings

--- auth/core/settings.py
+++ auth/core/settings.py
@@ -1,5 +1,6 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field
+import aioredis
 
 
 class DbSettings(BaseSettings):
@@ -27,9 +28,24 @@ class JWTSettings(BaseSettings):
         extra="allow"
     )
 
+
+class RedisSettings(BaseSettings):
+    redis_url: str = Field(validation_alias="REDIS_URL")
+    redis_port: str = Field(validation_alias="REDIS_PORT")
+    redis_password: str = Field(validation_alias="REDIS_PASSWORD")
+
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8',
+        case_sensitive=True,
+        extra="allow"
+    )
+
+
 class Settings(BaseSettings):
     db: DbSettings = DbSettings()
     jwt: JWTSettings = JWTSettings()
+    redis: RedisSettings = RedisSettings()
 
 
 settings = Settings()
