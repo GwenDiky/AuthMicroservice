a61a624248f8b6b8590d42227076667bdee320ba
Tamagil <tamagiltamagil@gmail.com>
2024-10-16 17:41:54 +0300

    feat:edited userrepo

--- auth/database/user_repo.py
+++ auth/database/user_repo.py
@@ -1,20 +1,13 @@
+from sqlalchemy.ext.asyncio import (
+    AsyncSession
+)
+
 from auth.database.repo import SqlAlchemyARepository
 from auth.database.user import User
-from sqlalchemy.ext.asyncio import (
-    AsyncSession, create_async_engine, async_sessionmaker
-)
-from auth.core.settings import settings
-from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy import engine
+
 
 class UserRepository(SqlAlchemyARepository):
     model = User
 
-    def __init__(self, async_engine: engine):
-        super().__init__(async_engine)
-
-async def get_user_repo() -> UserRepository:
-    return UserRepository(create_async_engine(
-        settings.db.db_url,
-        echo=True
-    ))
+    def __init__(self, db: AsyncSession):
+        super().__init__(db)
