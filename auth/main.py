45b87c9941c7584e9b009e553f18aae7006e9146
Tamagil <tamagiltamagil@gmail.com>
2024-10-16 16:04:28 +0300

    feat:moved routers to services

--- auth/main.py
+++ auth/main.py
@@ -1,11 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
-from auth.api.routers.user_routers import user_router
-from auth.database.user_repo import UserRepository
-import asyncio
-from auth.core.settings import settings
-from fastapi.middleware.cors import CORSMiddleware
-
+from auth.services.user import user_router
 
 app = FastAPI()
 app.include_router(user_router, prefix="/api/user", tags=['authentication'])
