ea060ea0b2c37250dba8b6fd741b9f750bdfdb38
Tamagil <tamagiltamagil@gmail.com>
2024-10-30 13:00:09 +0300

    feat:deleted unusless mail router

--- auth/api/routers.py
+++ auth/api/routers.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
-from auth.api.handlers.mail import mail_router
 from auth.api.handlers.user import user_router
 
 app = FastAPI()
 app.include_router(user_router, prefix="/api/user", tags=['authentication'])
-app.include_router(mail_router, prefix="/messages", tags=['messages'])
